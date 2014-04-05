7b50b72e97e1b679c8ae303afc10574c0f6ccaa8
gianlucadonato <gian.dnt@gmail.com>
2014-04-05 21:39:47 +0200

    add user local signup & omniauth facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 VeneziaUnica::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'homepage#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
