c8ce97217d61e0f94fd7d8f697a826746a4093c1
gianlucadonato <gian.dnt@gmail.com>
2014-04-05 15:11:04 +0200

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VeneziaUnica::Application.config.secret_key_base = '52719bb5f17b323f5762ed12cdf8d1c3e0d202991ab02642ada6ad5579a0c118d65345a6bf137e3e65d2b5ee092f4565314feb9ee1063a34f556cba591f3cba1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VeneziaUnica::Application.config.session_store :cookie_store, key: '_venezia_unica_session'
