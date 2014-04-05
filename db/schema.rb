7a0943f84d3d11d0ec10d7151c1413dbe79a9372
gianlucadonato <gian.dnt@gmail.com>
2014-04-05 22:04:25 +0200

    [layout] add settings and logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140405185821) do
+ActiveRecord::Schema.define(version: 20140405194146) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140405185821) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
