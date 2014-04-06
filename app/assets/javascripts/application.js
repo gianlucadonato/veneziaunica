7f6e429744d59fc2a523b903db23886fa64bbcb1
gianlucadonato <gian.dnt@gmail.com>
2014-04-06 04:53:34 +0200

    fix index layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,38 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap.min
+//= require bootstrap-datepicker
 //= require fonts
 //= require_tree .
+
+"use strict";
+
+
+$( document ).ready(function() {
+  
+	var nowTemp = new Date();
+	var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+	 
+	var checkin = $('#dp1').datepicker({
+	  onRender: function(date) {
+	    return date.valueOf() < now.valueOf() ? 'disabled' : '';
+	  }
+	}).on('changeDate', function(ev) {
+	  if (ev.date.valueOf() > checkout.date.valueOf()) {
+	    var newDate = new Date(ev.date)
+	    newDate.setDate(newDate.getDate() + 1);
+	    checkout.setValue(newDate);
+	  }
+	  checkin.hide();
+	  $('#dp2')[0].focus();
+	}).data('datepicker');
+
+	var checkout = $('#dp2').datepicker({
+	  onRender: function(date) {
+	    return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+	  }
+	}).on('changeDate', function(ev) {
+	  checkout.hide();
+	}).data('datepicker');
+
+});
